1c9cfb93bd8ae52abf3222c8d8708c9015de3c9d
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2021-11-26 11:21:55 +0100

    Update Product.test.js

--- tests/Product.test.js
+++ tests/Product.test.js
@@ -4,12 +4,10 @@ import Product from "../components/Product";
 
 describe("Product with a description", () => {
   it("Should detect if there is a product with a description", () => {
+
     render(<Product />);
 
-    const headingPro = screen.getByText(/Product component/i);
-    const headingDes = screen.getByText(/Description component/i);
-
-    expect(headingPro).toBeInTheDocument();
-    expect(headingDes).toBeInTheDocument();
+    expect(screen.getByText(/Product/i)).toBeInTheDocument();
+    expect(screen.getByText(/Description/i)).toBeInTheDocument();
   });
 });
