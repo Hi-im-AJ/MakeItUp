4c11abbd47c0c6b2a84f2f90f7c0ac3e4f7dc082
lostbutstillimproving <mike6353@edu.easj.dk>
2021-11-30 16:55:49 +0100

    implement search functionality

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,18 +1,18 @@
-import React, { useContext } from "react";
-import ProductContext from "../context/product/ProductContext";
+import {InstantSearch, SearchBox, Hits} from 'react-instantsearch-dom';
+import searchClient from "../lib/algolia";
 
-const Search = () => {
-  const productContext = useContext(ProductContext);
-  const { setSearchInput } = productContext;
-
-  const onChange = (e) => setSearchInput(e.target.value);
-
-  return (
-    <div>
-      <input onChange={(e) => onChange(e)} id="searchInput" name="searchInput" type="text" placeholder="Search..." />
-      <button id="submitButton">Search</button>
-    </div>
-  );
-};
+const Search = () => (
+    <InstantSearch searchClient={searchClient} indexName="products">
+        <SearchBox />
+        <Hits hitComponent={Hit} />
+    </InstantSearch>
+);
+const Hit =({hit}) => {
+    return (
+        <article>
+            <p>{hit.firstname}</p>
+        </article>
+    );
+}
 
 export default Search;
