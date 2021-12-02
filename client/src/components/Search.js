8c64faf0df37881e300fc7d3c90fbe99681c53c0
lostbutstillimproving <mike6353@edu.easj.dk>
2021-12-02 10:25:33 +0100

    implement search features: refinementlist, hits per page, pagination

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,11 +1,36 @@
-import {InstantSearch, SearchBox, Hits} from 'react-instantsearch-dom';
+import {
+    InstantSearch,
+    SearchBox,
+    Hits,
+    HitsPerPage,
+    Pagination,
+    RefinementList, CurrentRefinements,
+} from 'react-instantsearch-dom';
 import searchClient from "../lib/algolia";
 
+
 const Search = () => (
+
     <InstantSearch searchClient={searchClient} indexName="products">
-        <SearchBox />
-        <Hits hitComponent={Hit} />
+
+        <RefinementList attribute="firstname" operator="anasasd" searchable="true"/>
+        <SearchBox
+            translations={{
+            submitTitle: 'Submit your search query.',
+            resetTitle: 'Clear your search query.',
+            placeholder: 'Search...',
+        }}/>
+        <Hits hitComponent={Hit}/>
+        <HitsPerPage
+            defaultRefinement={5}
+            items={[
+                { value: 1, label: 'Show 1 hits' },
+                { value: 5, label: 'Show 5 hits' },
+            ]}
+        />
+        <Pagination />
     </InstantSearch>
+
 );
 const Hit =({hit}) => {
     return (
