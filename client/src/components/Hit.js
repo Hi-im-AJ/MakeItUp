08ffe2984d8861ccff7bbaa6c67e914ba4425390
AJ <38241145+Hi-im-AJ@users.noreply.github.com>
2021-12-10 14:27:52 +0100

    Implement add to cart from product list functionality

--- client/src/components/Hit.js
+++ client/src/components/Hit.js
@@ -1,9 +1,16 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import { Link } from "react-router-dom";
 import NoImage from "../assets/noImage.png";
+import CartContext from "../context/cart/CartContext";
+import commerce from "../lib/commerce";
 
 const Hit = ({ hit }) => {
   const { name, image, price, objectID } = hit;
+
+  const { setCart } = useContext(CartContext);
+
+  const addToCart = () => commerce.cart.add(objectID).then(({ cart }) => setCart(cart));
+
   return (
     <Fragment>
       <Link to={"product/" + objectID} id={objectID} key={objectID}>
@@ -11,7 +18,9 @@ const Hit = ({ hit }) => {
         <p>{name}</p>
         <p>{price.formatted_with_code}</p>
       </Link>
-      <button className="btn">Add To Cart</button>
+      <button className="btn" onClick={addToCart}>
+        Add To Cart
+      </button>
     </Fragment>
   );
 };
