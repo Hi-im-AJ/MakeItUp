da6e1440eb5ea207a8ce94194dc125c268162bab
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2021-12-10 12:20:33 +0100

    cleanup

--- client/src/lib/algolia.js
+++ client/src/lib/algolia.js
@@ -1,4 +1,5 @@
 import algoliasearch from 'algoliasearch';
+
 const searchClient = algoliasearch(process.env.REACT_APP_ALGOLIA_APP_ID, process.env.REACT_APP_ALGOLIA_SEARCH_API_KEY);
 
 export default searchClient
