fa79f20bd933cdc3cb1c2c8c93b2903b56b83845
AJ <38241145+Hi-im-AJ@users.noreply.github.com>
2021-12-10 12:37:29 +0100

    Add hit card improved styling

--- client/src/components/Hit.js
+++ client/src/components/Hit.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
+import NoImage from "../assets/noImage.png";
 
 const Hit = ({ hit }) => {
   const { name, image, price, objectID } = hit;
   return (
     <Fragment>
       <Link to={"product/" + objectID} id={objectID} key={objectID}>
-        {image ? <img src={image.url} alt={name} /> : null}
+        {image ? <img src={image.url} alt={name} /> : <img src={NoImage} alt={name} />}
         <p>{name}</p>
         <p>{price.formatted_with_code}</p>
       </Link>
