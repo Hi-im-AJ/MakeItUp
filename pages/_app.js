c3429f50fba5fc968fcc5f4d9adbe3f5442f0fb8
lostbutstillimproving <mike6353@edu.easj.dk>
2021-12-26 00:48:54 +0100

    Tilføj header til _app for seo

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,24 @@ import "../styles/main.css";
 import SearchState from "../context/search/SearchState";
 import CartState from "../context/cart/CartState";
 import Navbar from "../components/Navbar";
+import {useRouter} from "next/router";
+import head from "next/Head"
 
 export default function ({ Component, pageProps }) {
+  const router = useRouter();
+
+  let canonicalUrl
+  if (process.env.production) {
+    canonicalUrl = (`https://make-it-up-hi-im-aj.vercel.app` + (router.asPath === "/" ? "": router.asPath)).split("?")[0];
+  } else {
+    canonicalUrl = (`localhost2` + (router.asPath === "/" ? "": router.asPath)).split("?")[0];
+  }
+
   return (
     <>
+      <head>
+        <link rel="canonical" href={canonicalUrl} />
+      </head>
       <SearchState>
         <CartState>
           <Navbar />
