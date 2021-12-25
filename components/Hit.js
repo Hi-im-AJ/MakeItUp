bdc5e95f5b0adda859697d56dc0a47249741655e
lostbutstillimproving <mike6353@edu.easj.dk>
2021-12-25 21:29:38 +0100

    Reimplement product page

--- components/Hit.js
+++ components/Hit.js
@@ -1,7 +1,9 @@
-import { useContext } from "react";
+import {Fragment, useContext} from "react";
 import Link from "next/link";
+import Image from 'next/image'
 import CartContext from "../context/cart/CartContext";
 import commerce from "../lib/commerce";
+import NoImage from "../public/assets/noImage.png";
 
 export default function ({ hit }) {
   const { name, image, price, objectID } = hit;
@@ -12,13 +14,11 @@ export default function ({ hit }) {
 
   return (
     <div key={objectID} id={objectID}>
-      <Link href={"product/" + objectID}>
-        <>
-          {image ? <img src={image.url} alt={name} /> : <img src="/assets/noImage.png" alt={name} />}
-          <p>{name}</p>
-          <p>{price.formatted_with_code}</p>
-        </>
+      <Link href={"product/" + objectID} >
+          {image ? <img src={image.url} alt={name} /> : <img src={NoImage} alt={name} />}
       </Link>
+      <p>{name}</p>
+      <p>{price.formatted_with_code}</p>
       <button className="btn" onClick={addToCart}>
         Add To Cart
       </button>
