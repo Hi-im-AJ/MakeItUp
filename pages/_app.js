3a3e4a3dce2124c4eb6df57628abe736a92270f4
Anders Højer Jørgensen <virtualzeth@gmail.com>
2021-12-26 20:00:39 +0100

    Restyle navbar !missing searchbar colors

--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,20 @@ import "../styles/main.css";
 import SearchState from "../context/search/SearchState";
 import CartState from "../context/cart/CartState";
 import Navbar from "../components/Navbar";
-import {useRouter} from "next/router";
-import {DefaultSeo} from "next-seo";
+import { useRouter } from "next/router";
+import { DefaultSeo } from "next-seo";
+import ThemeProvider, { mainTheme } from "../lib/themes";
 
 export default function ({ Component, pageProps }) {
-
   const router = useRouter();
 
-  //Fordi Anders elsker ternaries
-  const canonicalUrl = process.env.NEXT_PUBLIC_ENV === "production" ? (`https://make-it-up-hi-im-aj.vercel.app`
-                                              + (router.asPath === "/" ? "": router.asPath))
-                                                .split("?")[0] : "localhost";
+  const canonicalUrl =
+    process.env.NEXT_PUBLIC_ENV === "production"
+      ? (`https://make-it-up-hi-im-aj.vercel.app` + (router.asPath === "/" ? "" : router.asPath)).split("?")[0]
+      : "localhost";
 
   return (
-    <>
+    <ThemeProvider theme={mainTheme}>
       <DefaultSeo canonical={canonicalUrl}>
         <link rel="canonical" href={canonicalUrl} />
       </DefaultSeo>
@@ -25,6 +25,6 @@ export default function ({ Component, pageProps }) {
           <Component {...pageProps} />
         </CartState>
       </SearchState>
-    </>
+    </ThemeProvider>
   );
 }
