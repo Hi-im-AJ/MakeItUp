d01f5a2ae457146be54c52a604d1a12865a93dfe
Mike <mike6353@edu.easj.dk>
2022-01-22 22:03:14 +0100

    fix stripped

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -37,13 +37,13 @@ export default ({ product }) => {
   const add = () => {
     addToCart(product.id)
   }
-  const strippedDescription = stripTags(product.description)
+
   return product ? (
 
 
     <Container maxWidth="lg">
       <Head key={product.id}>
-        <meta name="description" content={strippedDescription} />
+        <meta name="description" content={stripTags(product.description)} />
       </Head>
       {loading && <CircularProgress sx={{
         position: "fixed",
@@ -67,7 +67,7 @@ export default ({ product }) => {
               <Typography sx={{ mb: 4 }} variant="h3">
                 {product.name}
               </Typography>
-              <Typography variant="subtitle1">{strippedDescription}</Typography>
+              <Typography variant="subtitle1">{stripTags(product.description)}</Typography>
               <CardActions sx={{ mt: 2 }}>
                 <Button onClick={add} variant="outlined">
                   Add To Cart
